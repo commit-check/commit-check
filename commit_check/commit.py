a385d79d1ba92ef3bb9cc99254de995ce09be50b
shenxianpeng <xianpeng.shen@gmail.com>
2023-02-17 11:12:20 +0800

    test: update check_commit_msg func

--- commit_check/commit.py
+++ commit_check/commit.py
@@ -1,5 +1,6 @@
 """Check git commit message formatting"""
 import re
+import os
 import sys
 from commit_check import YELLOW, RESET_COLOR, PASS, FAIL
 from commit_check.util import get_commits_info, print_error_message, print_suggestion
@@ -13,9 +14,12 @@ def check_commit_msg(checks: list) -> int:
                     f"{YELLOW}Not found regex for commit message. skip checking.{RESET_COLOR}",
                 )
                 return PASS
-            commit_msg_file = sys.argv[1]
-            with open(commit_msg_file, 'r') as f:
-                commit_msg = f.read()
+            if os.environ.get("GIT_HOOK_MODE") == "hook":
+                commit_msg_file = sys.argv[1]
+                with open(commit_msg_file, 'r') as f:
+                    commit_msg = f.read()
+            else:
+                commit_msg = str(get_commits_info("s"))
             result = re.match(check['regex'], commit_msg)
             if result is None:
                 print_error_message(
