53a2a853d4ac0ae36a65e730dfe9c04139589776
shenxianpeng <xianpeng.shen@gmail.com>
2023-12-26 15:31:20 +0800

    test: add more tests

--- tests/commit_test.py
+++ tests/commit_test.py
@@ -1,5 +1,5 @@
 from commit_check import PASS, FAIL
-from commit_check.commit import check_commit_msg, get_default_commit_msg_file, read_commit_msg
+from commit_check.commit import check_commit_msg, get_default_commit_msg_file, read_commit_msg, check_commit_signoff
 
 # used by get_commits_info mock
 FAKE_BRANCH_NAME = "fake_commits_info"
@@ -10,29 +10,42 @@ MSG_FILE = '.git/COMMIT_EDITMSG'
 
 
 def test_get_default_commit_msg_file(mocker):
-    mocker.patch("commit_check.util.cmd_output", return_value="git_dir_output")
+    m_commit_msg_file = mocker.patch(
+        "commit_check.util.cmd_output",
+        return_value="git_dir_output"
+    )
     retval = get_default_commit_msg_file()
     assert retval == ".git/COMMIT_EDITMSG"
+    m_commit_msg_file.call_count == 1
 
 
-def test_read_commit_msg_file_found(tmp_path):
-    # Create a temporary file with content
-    commit_msg_file = tmp_path / "commit_msg.txt"
-    content = "Test commit message"
-    commit_msg_file.write_text(content)
+def test_get_default_commit_msg_file_not_found(mocker):
+    m_commit_msg_file = mocker.patch(
+        "commit_check.util.cmd_output",
+        return_value="git_dir_output"
+    )
+    retval = get_default_commit_msg_file()
+    assert retval == ".git/COMMIT_EDITMSG"
+    m_commit_msg_file.call_count == 1
+
+
+def test_read_commit_msg_from_existing_file(tmp_path):
+    # Create a temporary file with a known content
+    commit_msg_content = "Test commit message content."
+    commit_msg_file = tmp_path / "test_commit_msg.txt"
+    commit_msg_file.write_text(commit_msg_content)
 
     result = read_commit_msg(commit_msg_file)
-    assert result == content
+    assert result == commit_msg_content
 
 
 def test_read_commit_msg_file_not_found(mocker):
-    mocker.patch(f'{LOCATION}.get_commits_info', return_value="Last commit message")
-    result = read_commit_msg("nonexistent_file.txt")
-    assert result == "Last commit message"
+    m_commits_info = mocker.patch('commit_check.util.get_commits_info', return_value='mocked_commits_info')
+    read_commit_msg("non_existent_file.txt")
+    assert m_commits_info.call_count == 0
 
 
 def test_check_commit_with_empty_checks(mocker):
-    # Must NOT call get_commits_info, re.match. with `checks` param with length 0.
     checks = []
     m_re_match = mocker.patch(
         "re.match",
@@ -44,7 +57,6 @@ def test_check_commit_with_empty_checks(mocker):
 
 
 def test_check_commit_with_different_check(mocker):
-    # Must NOT call get_commit_info, re.match with not `message`.
     checks = [{
         "check": "branch",
         "regex": "dummy_regex"
@@ -59,7 +71,6 @@ def test_check_commit_with_different_check(mocker):
 
 
 def test_check_commit_with_len0_regex(mocker, capfd):
-    # Must NOT call get_commits_info, re.match with `regex` with length 0.
     checks = [
         {
             "check": "message",
@@ -78,7 +89,6 @@ def test_check_commit_with_len0_regex(mocker, capfd):
 
 
 def test_check_commit_with_result_none(mocker):
-    # Must call print_error_message, print_suggestion when re.match returns NONE.
     checks = [{
         "check": "message",
         "regex": "dummy_regex",
@@ -100,3 +110,54 @@ def test_check_commit_with_result_none(mocker):
     assert m_re_match.call_count == 1
     assert m_print_error_message.call_count == 1
     assert m_print_suggestion.call_count == 1
+
+
+def test_check_commit_signoff(mocker):
+    checks = [{
+        "check": "commit_signoff",
+        "regex": "dummy_regex",
+        "error": "error",
+        "suggest": "suggest"
+    }]
+    m_re_match = mocker.patch(
+        "re.match",
+        return_value=None
+    )
+    m_print_error_message = mocker.patch(
+        f"{LOCATION}.print_error_message"
+    )
+    m_print_suggestion = mocker.patch(
+        f"{LOCATION}.print_suggestion"
+    )
+    retval = check_commit_signoff(checks)
+    assert retval == FAIL
+    assert m_re_match.call_count == 1
+    assert m_print_error_message.call_count == 1
+    assert m_print_suggestion.call_count == 1
+
+
+def test_check_commit_signoff_with_empty_regex(mocker):
+    checks = [{
+        "check": "commit_signoff",
+        "regex": "",
+        "error": "error",
+        "suggest": "suggest"
+    }]
+    m_re_match = mocker.patch(
+        "re.match",
+        return_value="fake_commits_info"
+    )
+    retval = check_commit_signoff(checks)
+    assert retval == PASS
+    assert m_re_match.call_count == 0
+
+
+def test_check_commit_signoff_with_empty_checks(mocker):
+    checks = []
+    m_re_match = mocker.patch(
+        "re.match",
+        return_value="fake_commits_info"
+    )
+    retval = check_commit_signoff(checks)
+    assert retval == PASS
+    assert m_re_match.call_count == 0
