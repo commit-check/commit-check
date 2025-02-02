cc9737edb5d75015bcdc7d797c29a94002cb110d
B McDonnell <bcm.mcdonnell@gmail.com>
2025-02-01 18:07:31 -0800

    style: revert automatic formatting

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,72 +1,49 @@
 import sys
 import pytest
 from commit_check.main import main
-from commit_check import DEFAULT_CONFIG, PASS, FAIL, author
+from commit_check import DEFAULT_CONFIG
 
 CMD = "commit-check"
 
 
 class TestMain:
-    @pytest.mark.parametrize(
-        "argv, check_commit_call_count, check_branch_call_count, check_author_call_count, check_commit_signoff_call_count, check_merge_base_call_count",
-        [
-            ([CMD, "--message"], 1, 0, 0, 0, 0),
-            ([CMD, "--branch"], 0, 1, 0, 0, 0),
-            ([CMD, "--author-name"], 0, 0, 1, 0, 0),
-            ([CMD, "--author-email"], 0, 0, 1, 0, 0),
-            ([CMD, "--commit-signoff"], 0, 0, 0, 1, 0),
-            ([CMD, "--merge-base"], 0, 0, 0, 0, 1),
-            ([CMD, "--message", "--author-email"], 1, 0, 1, 0, 0),
-            ([CMD, "--branch", "--message"], 1, 1, 0, 0, 0),
-            ([CMD, "--author-name", "--author-email"], 0, 0, 2, 0, 0),
-            ([CMD, "--message", "--branch", "--author-email"], 1, 1, 1, 0, 0),
-            (
-                [CMD, "--branch", "--message", "--author-name", "--author-email"],
-                1,
-                1,
-                2,
-                0,
-                0,
-            ),
-            (
-                [
-                    CMD,
-                    "--message",
-                    "--branch",
-                    "--author-name",
-                    "--author-email",
-                    "--commit-signoff",
-                    "--merge-base",
-                ],
-                1,
-                1,
-                2,
-                1,
-                1,
-            ),
-            ([CMD, "--dry-run"], 0, 0, 0, 0, 0),
-        ],
-    )
+    @pytest.mark.parametrize("argv, check_commit_call_count, check_branch_call_count, check_author_call_count, check_commit_signoff_call_count, check_merge_base_call_count", [
+        ([CMD, "--message"], 1, 0, 0, 0, 0),
+        ([CMD, "--branch"], 0, 1, 0, 0, 0),
+        ([CMD, "--author-name"], 0, 0, 1, 0, 0),
+        ([CMD, "--author-email"], 0, 0, 1, 0, 0),
+        ([CMD, "--commit-signoff"], 0, 0, 0, 1, 0),
+        ([CMD, "--merge-base"], 0, 0, 0, 0, 1),
+        ([CMD, "--message", "--author-email"], 1, 0, 1, 0, 0),
+        ([CMD, "--branch", "--message"], 1, 1, 0, 0, 0),
+        ([CMD, "--author-name", "--author-email"], 0, 0, 2, 0, 0),
+        ([CMD, "--message", "--branch", "--author-email"], 1, 1, 1, 0, 0),
+        ([CMD, "--branch", "--message", "--author-name", "--author-email"], 1, 1, 2, 0, 0),
+        ([CMD, "--message", "--branch", "--author-name", "--author-email", "--commit-signoff", "--merge-base"], 1, 1, 2, 1, 1),
+        ([CMD, "--dry-run"], 0, 0, 0, 0, 0),
+    ])
     def test_main(
-        self,
-        mocker,
-        argv,
-        check_commit_call_count,
-        check_branch_call_count,
-        check_author_call_count,
-        check_commit_signoff_call_count,
-        check_merge_base_call_count,
+            self,
+            mocker,
+            argv,
+            check_commit_call_count,
+            check_branch_call_count,
+            check_author_call_count,
+            check_commit_signoff_call_count,
+            check_merge_base_call_count,
     ):
         mocker.patch(
             "commit_check.main.validate_config",
-            return_value={"checks": [{"check": "dummy_check_type"}]},
+            return_value={
+                "checks": [
+                    {"check": "dummy_check_type"}
+                ]
+            }
         )
         m_check_commit = mocker.patch("commit_check.commit.check_commit_msg")
         m_check_branch = mocker.patch("commit_check.branch.check_branch")
         m_check_author = mocker.patch("commit_check.author.check_author")
-        m_check_commit_signoff = mocker.patch(
-            "commit_check.commit.check_commit_signoff"
-        )
+        m_check_commit_signoff = mocker.patch("commit_check.commit.check_commit_signoff")
         m_check_merge_base = mocker.patch("commit_check.branch.check_merge_base")
         sys.argv = argv
         main()
@@ -79,14 +56,16 @@ class TestMain:
     def test_main_help(self, mocker, capfd):
         mocker.patch(
             "commit_check.main.validate_config",
-            return_value={"checks": [{"check": "dummy_check_type"}]},
+            return_value={
+                "checks": [
+                    {"check": "dummy_check_type"}
+                ]
+            }
         )
         m_check_commit = mocker.patch("commit_check.commit.check_commit_msg")
         m_check_branch = mocker.patch("commit_check.branch.check_branch")
         m_check_author = mocker.patch("commit_check.author.check_author")
-        m_check_commit_signoff = mocker.patch(
-            "commit_check.commit.check_commit_signoff"
-        )
+        m_check_commit_signoff = mocker.patch("commit_check.commit.check_commit_signoff")
         m_check_merge_base = mocker.patch("commit_check.branch.check_merge_base")
         sys.argv = ["commit-check", "--h"]
         with pytest.raises(SystemExit):
@@ -102,14 +81,16 @@ class TestMain:
     def test_main_version(self, mocker):
         mocker.patch(
             "commit_check.main.validate_config",
-            return_value={"checks": [{"check": "dummy_check_type"}]},
+            return_value={
+                "checks": [
+                    {"check": "dummy_check_type"}
+                ]
+            }
         )
         m_check_commit = mocker.patch("commit_check.commit.check_commit_msg")
         m_check_branch = mocker.patch("commit_check.branch.check_branch")
         m_check_author = mocker.patch("commit_check.author.check_author")
-        m_check_commit_signoff = mocker.patch(
-            "commit_check.commit.check_commit_signoff"
-        )
+        m_check_commit_signoff = mocker.patch("commit_check.commit.check_commit_signoff")
         m_check_merge_base = mocker.patch("commit_check.branch.check_merge_base")
         sys.argv = ["commit-check", "--v"]
         with pytest.raises(SystemExit):
@@ -121,7 +102,10 @@ class TestMain:
         assert m_check_merge_base.call_count == 0
 
     def test_main_validate_config_ret_none(self, mocker):
-        mocker.patch("commit_check.main.validate_config", return_value={})
+        mocker.patch(
+            "commit_check.main.validate_config",
+            return_value={}
+        )
         m_check_commit = mocker.patch("commit_check.commit.check_commit_msg")
         mocker.patch("commit_check.branch.check_branch")
         mocker.patch("commit_check.author.check_author")
