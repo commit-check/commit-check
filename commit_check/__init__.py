a237cda9cd94d40cb8caee59b7aa878e4589b5c8
Kohányi Róbert <kohanyi.robert@gmail.com>
2025-02-23 18:00:11 +0100

    fix: allow accented letters in author name (#221)
    * fix: allow accented letters in author name
    
    Allow accented letters (e.g. áéó, etc.) in an
    author's name.
    
    * fix: remove duplicate unicode regex range
    
    * fix: add broader regex range to .yml as well
    
    * fix: remove duplicate regex key

--- commit_check/__init__.py
+++ commit_check/__init__.py
@@ -32,7 +32,7 @@ DEFAULT_CONFIG = {
         },
         {
             'check': 'author_name',
-            'regex': r'^[A-Za-z ,.\'-]+$|.*(\[bot])',
+            'regex': r'^[A-Za-zÀ-ÖØ-öø-ÿ\u0100-\u017F\u0180-\u024F ,.\'-]+$|.*(\[bot])',
             'error': 'The committer name seems invalid',
             'suggest': 'run command `git config user.name "Your Name"`',
         },
